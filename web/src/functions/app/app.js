e606b061b419ba6196e88e103264dcadda113cbe
Yuki Hirai <yukihirai0505@gmail.com>
2018-08-28 11:36:24 +0900

    Add express & next to firebase functions https

--- web/src/functions/app/app.js
+++ web/src/functions/app/app.js
@@ -1,25 +1,24 @@
-import * as functions from 'firebase-functions'
 import next from 'next'
+import express from 'express'
+import cors from 'cors'
+import * as functions from 'firebase-functions'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev, conf: { distDir: 'next' } })
 const handle = app.getRequestHandler()
 
-const nextApp = functions.https.onRequest((request, response) => {
-  console.log('File: ' + request.originalUrl)
-  console.log('dev:', dev)
-  // log the page.js file or resource being requested
-  console.dir(request)
-  const queryParams = { id: request.params.id }
-  console.log('queryParams:', queryParams)
-  if (queryParams.id) {
-    const actualPath = `/post`
-    return app
-      .prepare()
-      .then(() => handle(request, response, actualPath, queryParams))
-  } else {
-    return app.prepare().then(() => handle(request, response))
-  }
+const server = express()
+server.use(cors())
+server.get('/p/:id', (req, res) => {
+  const actualPage = '/post'
+  const queryParams = { id: req.params.id }
+  app.render(req, res, actualPage, queryParams)
+})
+server.get('*', (req, res) => handle(req, res))
+
+const nextApp = functions.https.onRequest(async (req, res) => {
+  await app.prepare()
+  return server(req, res)
 })
 
 export { nextApp }
