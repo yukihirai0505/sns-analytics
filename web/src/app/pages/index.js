c65cbd16e86ed7852f2d28a49883b85fcc26ddc3
Yuki Hirai <yukihirai0505@gmail.com>
2018-09-03 18:51:21 +0900

    Refactoring

--- web/src/app/pages/index.js
+++ web/src/app/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Page from '../layouts/main'
+import TwitterEmbed from '../components/twitterEmbed'
 import Link from 'next/link'
 import { auth, providerTwitter, configs } from '../config'
 import cookies from 'next-cookies'
@@ -109,58 +110,7 @@ class Index extends Component {
           ))}
         </ul>
         {tweets.map((twitter, index) => {
-          const twitterLink = `https://twitter.com/${
-            twitter.user.screen_name
-          }/statuses/${twitter.id_str}`
-          return (
-            <blockquote className="twitter-tweet" key={index}>
-              <p className="ja" dir="ltr">
-                {twitter.text}{' '}
-                <span dangerouslySetInnerHTML={{ __html: twitter.source }} />
-              </p>
-              — {twitter.user.name} (@
-              {twitter.user.screen_name}){' '}
-              <a href={twitterLink}>{twitter.created_at}</a>
-              <style jsx>
-                {`
-                  blockquote.twitter-tweet {
-                    display: inline-block;
-                    font-family: 'Helvetica Neue', Roboto, 'Segoe UI', Calibri,
-                      sans-serif;
-                    font-size: 12px;
-                    font-weight: bold;
-                    line-height: 16px;
-                    border-color: #eee #ddd #bbb;
-                    border-radius: 5px;
-                    border-style: solid;
-                    border-width: 1px;
-                    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
-                    margin: 10px 5px;
-                    padding: 0 16px 16px 16px;
-                    max-width: 468px;
-                  }
-
-                  blockquote.twitter-tweet p {
-                    font-size: 16px;
-                    font-weight: normal;
-                    line-height: 20px;
-                  }
-
-                  blockquote.twitter-tweet a {
-                    color: inherit;
-                    font-weight: normal;
-                    text-decoration: none;
-                    outline: 0 none;
-                  }
-
-                  blockquote.twitter-tweet a:hover,
-                  blockquote.twitter-tweet a:focus {
-                    text-decoration: underline;
-                  }
-                `}
-              </style>
-            </blockquote>
-          )
+          return <TwitterEmbed twitter={twitter} index={index} />
         })}
         {user ? (
           <div>
