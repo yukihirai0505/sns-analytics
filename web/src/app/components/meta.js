91d9f455ce33585266c165112a2aee74899c78fb
Yuki Hirai <yukihirai0505@gmail.com>
2018-09-03 19:17:33 +0900

    Fix firebase hosting error

--- web/src/app/components/meta.js
+++ web/src/app/components/meta.js
@@ -7,7 +7,5 @@ export default () => (
       <meta charSet="utf-8" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
     </Head>
-    <style jsx global>{`
-    `}</style>
   </div>
 )
